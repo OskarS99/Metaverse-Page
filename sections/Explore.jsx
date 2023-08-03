debe2360ca88e37afb6d7deabb909e4403d8bcec
Oskar Siry <oskarsiry99@gmail.com>
2023-08-03 15:43:45 +0200

    code formatted

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,13 +1,13 @@
-"use client";
-import { useState } from "react";
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { staggerContainer } from "../utils/motion";
-import { ExploreCard, TitleText, TypingText } from "../components";
-import { exploreWorlds } from "../constants";
+'use client';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { ExploreCard, TitleText, TypingText } from '../components';
+import { exploreWorlds } from '../constants';
 
 const Explore = () => {
-  const [active, setActive] = useState("world-2");
+  const [active, setActive] = useState('world-2');
   return (
     <section className={`${styles.paddings}`} id="explore">
       <motion.div
