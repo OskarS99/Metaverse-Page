4211dd344face66ef0e3d6f419ffa294bd56b443
Oskar Siry <oskarsiry99@gmail.com>
2023-08-03 15:35:09 +0200

    pathname to image changed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => (
     <div className="absolute w-[50%] inset-0" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
-        src="../public/search.svg"
+        src="search.svg"
         alt="search"
         className="w-[24px] h-24px object-contain"
       />
