705444f7ecd64d584decc8c333052e14a7bbce11
Oskar Siry <oskarsiry99@gmail.com>
2023-09-25 11:18:24 +0200

    code formatted and height image changed

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -10,12 +10,10 @@ const Feedback = () => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: 'false', amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
-    >
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}>
       <motion.div
         variants={fadeIn('right', 'tween', 0.2, 1)}
-        className="flex-[0.5] lg:max-w-[370px] flex justtify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
-      >
+        className="flex-[0.5] lg:max-w-[370px] flex justtify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative">
         <div className="feedback-gradient" />
         <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
@@ -33,18 +31,16 @@ const Feedback = () => (
       </motion.div>
       <motion.div
         variants={fadeIn('left', 'tween', 0.2, 1)}
-        className="relative flex-1 flex justify-center items-center"
-      >
+        className="relative flex-1 flex justify-center items-center">
         <img
           src="planet-09.png"
           alt="planet-09"
-          className="w-full lg:h-[160px] h-auto min-h-[210px] object-cover rounded-[32px]"
+          className="w-full  h-auto min-h-[210px] object-cover rounded-[32px]"
         />
 
         <motion.div
           variants={zoomIn(0.4, 1)}
-          className="lg:block hidden absolute -left-[10%] top-[3%]"
-        >
+          className="lg:block hidden absolute -left-[10%] top-[3%]">
           <img
             src="stamp.png"
             alt="stamp"
